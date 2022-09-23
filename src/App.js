53e1c62421de8bfd7e20def58f4593573878726c
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2022-09-23 20:53:55 +0530

    react image preview

--- src/App.js
+++ src/App.js
@@ -1,99 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { collection, deleteDoc, onSnapshot, query, QuerySnapshot, addDoc,doc,updateDoc } from 'firebase/firestore';
-import { AiOutlinePlus } from 'react-icons/ai';
-import { db } from './firebase/firebase';
-import Todo from './firebase/Todo';
-
-const style = {
-	bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-	container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
-	heading: `text-3xl font-bold text-center text-gray-800 p-2`,
-	form: `flex justify-between`,
-	input: `border p-2 w-full text-xl`,
-	button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-	count: `text-center p-2`,
-};
-
+import React, { useEffect, useRef, useState } from 'react';
 const App = () => {
-	const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
-  console.log("🚀 ~ file: App.js ~ line 20 ~ App ~ input", input)
-
-	// create todo
-	const createTodo = async (e) => {
-		e.preventDefault(e);
-		if (input === '') {
-			alert('Please enter a valid todo');
-			return;
-		}
-		await addDoc(collection(db, 'todos'), {
-			text: input,
-			completed: false,
-		});
-		setInput('');
-	};
-
-	// read todo
+	const [image, setImage] = useState();
+	const [preview, setPreview] = useState();
+	const fileInputRef = useRef(null);
 	useEffect(() => {
-		const q = query(collection(db, 'todos'));
-		const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-			let todosArr = [];
-			QuerySnapshot.forEach((doc) => {
-				todosArr.push({ ...doc.data(), id: doc.id });
-			});
-			setTodos(todosArr);
-		});
-		return () => unsubscribe();
-	}, []);
-	// Update todo in firebase
-	const toggleComplete = async (todo) => {
-		await updateDoc(doc(db, 'todos', todo.id), {
-			completed: !todo.completed,
-		});
-	};
-
-	// Delete todo
-	const deleteTodo = async (id) => {
-		await deleteDoc(doc(db, 'todos', id));
-	};
-
+		if (image) {
+			const reader = new FileReader();
+			reader.onloadend = () => {
+				setPreview(reader.result);
+			};
+			reader.readAsDataURL(image);
+		} else {
+			setPreview(null);
+		}
+	}, [image]);
 	return (
-		<div className={style.bg}>
-			<div className={style.container}>
-				<h3 className={style.heading}>Todo App</h3>
-				<form onSubmit={createTodo} className={style.form}>
-					<input
-						value={input}
-						onChange={(e) => setInput(e.target.value)}
-						className={style.input}
-						type="text"
-						placeholder="Add todo"
-					/>
-					<button className={style.button}>
-						{' '}
-						<AiOutlinePlus size={30} />{' '}
+		<div>
+			<form>
+				{preview ? (
+					<img src={preview} alt='' onClick={() => {
+						setImage(null)
+				}} />
+				) : (
+					<button
+						onClick={(event) => {
+							event.preventDefault();
+							fileInputRef.current.click();
+						}}
+					>
+						Add image
 					</button>
-				</form>
-				<ul>
-					{todos.map((todo, index) => {
-						return (
-							<>
-								<Todo
-									key={index}
-									todo={todo}
-									deleteTodo={deleteTodo}
-									toggleComplete={toggleComplete}
-								/>
-							</>
-						);
-					})}
-				</ul>
-				{todos.length < 1 ? null : (
-					<p
-						className={style.count}
-					>{`You have ${todos.length} todos`}</p>
 				)}
-			</div>
+
+				<input
+					type="file"
+					style={{ display: 'none' }}
+					accept="image/*"
+					ref={fileInputRef}
+					onChange={(event) => {
+						const file = event.target.files[0];
+						if (file && file.type.substr(0, 5) === 'image') {
+							setImage(file);
+						} else {
+							setImage(null);
+						}
+					}}
+				/>
+			</form>
 		</div>
 	);
 };
